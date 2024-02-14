296724d72c9b961c83581379b79ff15cddb51ad3
Po (Dragon Warrior) <149895321+PranavGujarathi@users.noreply.github.com>
2024-02-14 14:37:37 +0000

    Basics

---
+++ 01_Basics/01_Basics.py
@@ -0,0 +1,95 @@
+
+# interactive mode
+# script mode
+
+# string = 'my name is pranav \
+#         i live in bombay  \n \
+#         my hobby is to do coding \n \
+#         how are you \n \
+#         what is your name'
+
+# string = 'my name is pranav \t i live in bombay  \t my hobby is to do coding'/
+
+string = "Helllo how are you"
+
+print(string,"Janak",sep="\n")
+
+
+a =10  #a is variable containg 10
+b=20
+# is 10 greater than 20
+# 10>20 return False
+
+c= a+b
+print(c)
+
+
+# print(a,b,aug="27")
+
+
+string = "Pranav"
+decimal_marks = 100.233   #float
+boolean_var1 = True
+boolean_var2 = False
+none_var = None
+print(boolean_var1)
+print(10<20)
+
+#Variable naming
+# alphabats, numbers, and _
+# $ $#@$ use nhhi kar sakthe
+# 12pranav = 10 -- error
+# python is case sensitive
+
+A =10
+a=20
+print(1.0e4)  #1.0 X 10^4
+
+print(2 + 10j)
+print("the type of given value is ",type(None))
+
+
+
+# DYNAMIC TYPING
+a="hello everyone"
+print(a)
+a=30
+print(a)
+
+true = 10
+
+
+
+a=10
+b=a
+print("berfore updating b ",a,b)
+a=30
+print(a,b)
+
+a = [1,2,3]
+b=a
+print(a,b)
+a[0] = 10  #mutable
+print(a,b)
+
+# s = "Pranav"   #non mutable
+# s[1] = "t"
+
+
+
+""""For
+ multi line 
+comments """
+
+
+
+'''
+single 
+quets also can be used 
+3 times
+'''
+
+
+s = "Pranav\"s car"
+print(s)
+
