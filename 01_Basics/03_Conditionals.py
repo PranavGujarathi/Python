c1db427b603d2ebb3256ce0091bfd9f90c0139a9
Po (Dragon Warrior) <149895321+PranavGujarathi@users.noreply.github.com>
2024-02-14 15:08:19 +0000

    Conditional statements

---
+++ 01_Basics/03_Conditionals.py
@@ -0,0 +1,51 @@
+
+# Conditional
+
+#if - else
+
+# a=10
+# if a==11:
+#     print("A is equal to 11")
+# else:
+#      print("A is not equal to 11")
+
+# print("Program finished")
+
+
+# elif ladder
+
+# a=1
+
+# if a==1:
+#     print("a is 1")
+# elif a==2:
+#     print("a is 2")
+# elif a==3:
+#     print("a is 3")
+# else:
+#     print("a is something bigger than 3")
+
+#Here when if or elif condition true 
+#vo baki ko dekhega bhi nahi
+
+
+# Multiple if
+# here whn if execute condition fir bhi wo aage dekhega
+
+# a=1
+# if a==1:
+#     print("a is 1")
+# if a==2:
+#     print("a is 2")
+# if a==3:
+#     print("a is 3")
+
+
+# if a%2==0:
+#     print("a is odd number")
+# else:
+#     print("halow")
+
+
+
+
